321d1b9e342c4065aefe0bf6ccf6a00b5240fa56
Maxim Safronov <1maximsafronov@gmail.com>
2022-06-28 16:12:15 +0700

    Добавляет базовые стили

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'normalize.css';
+import './styles/index.scss';
 import App from './components/app/app';
 
 const root = ReactDOM.createRoot(
